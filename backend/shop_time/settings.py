ff97c5728c7e28d965bbc5a4d8754010a652faa0
Diego <dmuinoo@gmail.com>
2022-01-13 14:23:19 +0100

    Private

--- backend/shop_time/settings.py
+++ backend/shop_time/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ko!8%7khb()jm0&7s4!kifck9&m1(x73256+oe_ox8es@%vc+e'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -103,7 +103,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'shoptime125@gmail.com'
-EMAIL_HOST_PASSWORD = 'nfminodrbmmvxtqj'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 
@@ -200,8 +200,8 @@ DJOSER = {
     }
 } 
 
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '225197140918-bsu1vj4cabh3na9a6057ul3qbhq22m04.apps.googleusercontent.com'
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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'
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = ''
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = ''
 SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = [
     'https://www.googleapis.com/auth/userinfo.email',
     'https://www.googleapis.com/auth/userinfo.profile',
@@ -209,8 +209,8 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = [
 ]
 SOCIAL_AUTH_GOOGLE_OAUTH2_EXTRA_DATA = ['first_name', 'last_name']
 
-SOCIAL_AUTH_FACEBOOK_KEY = '896851874358406'
-SOCIAL_AUTH_FACEBOOK_SECRET = 'eee634aa329359b2f954cd54d8241749'
+SOCIAL_AUTH_FACEBOOK_KEY = ''
+SOCIAL_AUTH_FACEBOOK_SECRET = ''
 SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
 SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
     'fields': 'email, first_name, last_name'
